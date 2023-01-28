87ab542b3b1e709a8d3f04f94f960eacb75f30b2
Ahmet Can KOCA <123681169+AhmetCK3319@users.noreply.github.com>
2023-01-28 23:39:10 +0300

    first test

---
+++ class_attribute.py
@@ -0,0 +1,28 @@
+class User:
+    active_users=0
+    
+    def __init__(self,username,name,surname,age):
+        self.username=username
+        self.name=name
+        self.surname=surname
+        self.age=age
+        User.active_users +=1
+    def username (self):
+        return f"{self.username}  " 
+    
+    def logout (self):
+        User.active_users -=1
+        return f"{self.username} çikiş yapti"
+       
+user1=User("ahmetck","ahmet can","koca",33)
+user2=User("ahmetak","ahmet can","koca",36)
+user3=User("ahmettk","ahmet can","koca",35)
+user4=User("ahmetpk","ahmet can","koca",34)
+user5=User("ahmetpk","ahmet can","koca",34)
+user6=User("ahmetpk","ahmet can","koca",34)
+print(f" aktif kullanici sayisi : {User.active_users}")    
+print(user2.logout())
+print(user1.logout())
+print(f" aktif kullanici sayisi : {User.active_users}")    
+      
+        
